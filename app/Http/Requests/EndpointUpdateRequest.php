9603bfdab6ba582869402c5a84a4168b6e685b43
alnahian2003 <a.alnahian2003@gmail.com>
2023-06-02 00:36:54 +0600

    EndpointUpdateRequest formatted using Pint

--- app/Http/Requests/EndpointUpdateRequest.php
+++ app/Http/Requests/EndpointUpdateRequest.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Validation\Rules\Enum;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
 
 class EndpointUpdateRequest extends FormRequest
 {
