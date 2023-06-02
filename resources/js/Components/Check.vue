53bfc75bf91c48e8c5382f2f60c997182f17046e
alnahian2003 <a.alnahian2003@gmail.com>
2023-06-02 17:12:09 +0600

    displaying response body modal

--- resources/js/Components/Check.vue
+++ resources/js/Components/Check.vue
@@ -1,7 +1,11 @@
 <script setup>
 import StatusBadge from "@/Components/StatusBadge.vue";
+import { ref } from "vue";
+import { VueFinalModal } from "vue-final-modal";
 
 const props = defineProps({ check: Object });
+
+const ShowResponseBodyModal = ref(false);
 </script>
 
 <template>
@@ -15,9 +19,31 @@ const props = defineProps({ check: Object });
             <StatusBadge :check="check" />
         </td>
         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-            <template v-if="check.response_body"> View </template>
+            <template v-if="check.response_body">
+                <button @click="ShowResponseBodyModal = true">View</button>
+            </template>
 
             <template v-else> - </template>
         </td>
     </tr>
+
+    <VueFinalModal
+        v-model="ShowResponseBodyModal"
+        classes="flex justify-center items-center pt-16 mx-4"
+        content-class="relative max-h-full rounded bg-white dark:bg-gray-800 dark:text-gray-100 w-full max-w-2xl p-4 md:p-6 space-y-4"
+        overlay-class="bg-gradient-to-r from-gray-800 to-gray-500 opacity-50"
+        :esc-to-close="true"
+    >
+        <h2
+            class="font-semibold text-lg text-gray-800 dark:text-gray-100 leading-tight"
+        >
+            Response Body
+        </h2>
+        <textarea
+            class="w-full block rounded border border-gray-300 outline-none"
+            rows="20"
+            :value="check.response_body"
+            readonly
+        ></textarea>
+    </VueFinalModal>
 </template>
